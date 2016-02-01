9f49e7ef6cdcd04ecd3beeb3dd52d3f1b0a4b466
Parag Bhatnagar <paragbhtngr@gmail.com>
2016-02-01 14:47:56 -0500

    body-parser for dealing with forms

--- todoapp1/myapp/app.js
+++ todoapp1/myapp/app.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
-
 var app = express();
+var bodyParser = require('body-parser');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', function(req, res) {
   res.render('home');
+});
+
+app.post('/user/register', function(req, res) {
+  res.send(req.body);
+  console.log('The user has the email address', req.body.email);
 });
 
 app.listen(3000, function () {
