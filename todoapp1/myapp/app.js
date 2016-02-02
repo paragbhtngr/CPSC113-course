2480698ae81483c46481e32d06a5f5d33a83a934
Parag Bhatnagar <paragbhtngr@gmail.com>
2016-02-02 00:28:58 -0500

    mongodb database created

--- todoapp1/myapp/app.js
+++ todoapp1/myapp/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
 var bodyParser = require('body-parser');
+var Users = require('./models/users.js');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -9,11 +10,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', function(req, res) {
-  res.render('home');
+  Users.count(function(err, users){
+    if(err) {
+      res.send('error getting users');
+    }else {
+      res.render('home', {userCount: users.length});
+    }
+  });
 });
 
 app.post('/user/register', function(req, res) {
-  res.send(req.body);
+  var newUser = new Users();
+  newUser.hashed_password = req.body.password;
+  newUser.email = req.body.email;
+  newUser.name = req.body.fl_name;
+  newUser.save(function(err){
+    if(err){
+      res.send('there was an error saving the user');
+    }else {
+      res.redirect('/');
+    }
+  });
   console.log('The user has the email address', req.body.email);
 });
 
